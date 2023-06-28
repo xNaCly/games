ab777f45e383524fb4af5ea03c3db4e3c21f4712
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-06-28 11:39:41 +0200

    feat(dino): main setup

--- dinosaur/main.c
+++ dinosaur/main.c
@@ -1,8 +1,15 @@
+#include "../lib/io.h"
 #include "../lib/term.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(void) {
   term_enable_raw_mode();
+  term_disable_buffering(stdout);
+  char c;
+  while (c = io_read_char(), c != -1) {
+    printf("%c", c);
+  }
   return EXIT_SUCCESS;
 }
